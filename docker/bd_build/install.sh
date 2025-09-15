3b35ed7dfb54fad0e94d5e1b2c16335c23bcdb9a
zhangjun <ibmzhangjun@139.com>
2025-09-15 15:36:54 +0800

    add realtime audio

--- docker/bd_build/install.sh
+++ docker/bd_build/install.sh
@@ -4,7 +4,7 @@ set -x
 apt-get update && DEBIAN_FRONTEND=noninteractive && \
 apt -y dist-upgrade && \
 apt install -y --no-install-recommends build-essential libssl-dev libffi-dev python3-dev net-tools libsasl2-dev curl wget procps git libnss3-tools python3-pip && \
-apt install -y software-properties-common  && add-apt-repository -y ppa:deadsnakes/ppa && apt install -y python3.10 && \
+apt install -y software-properties-common  && add-apt-repository -y ppa:deadsnakes/ppa && apt install -y python3.10.10 && \
 rm /usr/bin/python && ln -s /usr/bin/python3.10 /usr/bin/python && \
 python -m pip install virtualenv && \
 cd /opt && \
